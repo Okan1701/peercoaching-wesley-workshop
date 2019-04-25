6d0b70ab2a891547a30c6adfa1b86e6cf4e80dcb
Vincent <0948125@hr.nl>
2019-04-25 11:24:27 +0200

    recycler yeet

--- app/src/main/java/coom/hr/opendag/MainActivity.java
+++ app/src/main/java/coom/hr/opendag/MainActivity.java
@@ -2,12 +2,37 @@ package coom.hr.opendag;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        spinner = findViewById(R.id.spinner);
+        //spinner.setOnItemSelectedListener();
+
+            spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view,  int pos, long id) {
+                    Toast.makeText(parent.getContext(), "OnItemSelectedListener : " + parent.getItemAtPosition(pos).toString(),
+                            Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parentView) {
+                    // your code here
+                }
+            });
+
+
+        }
     }
-}
+
