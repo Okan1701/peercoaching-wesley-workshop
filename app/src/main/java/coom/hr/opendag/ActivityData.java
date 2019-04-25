6e60cacc209d4179db681098ac192b548e500f30
Okan Emeni <okanemeni@outlook.com>
2019-04-25 11:35:15 +0200

    Linked spinner to recycler view

--- app/src/main/java/coom/hr/opendag/ActivityData.java
+++ app/src/main/java/coom/hr/opendag/ActivityData.java
@@ -32,18 +32,18 @@ public class ActivityData {
     }
 
     // Generate data
-    public static ActivityData[] generateData(ActivityData.Group group) {
+    public static ActivityData[] generateData(String group) {
         ActivityData[] data = new ActivityData[4];
 
         switch (group) {
-            case Informatica:
+            case "Informatica":
                 data[0] = new ActivityData("Presentatie", "09:00");
                 data[1] = new ActivityData("Workshop", "9:30");
                 data[2] = new ActivityData("Presentatie", "12:00");
                 data[3] = new ActivityData("Workshop", "12:30");
                 break;
 
-            case TechnischeInformatica:
+            case "Technische Informatica":
                 data[0] = new ActivityData("Presentatie", "15:00");
                 data[1] = new ActivityData("Workshop", "15:30");
                 data[2] = new ActivityData("Presentatie", "17:00");
--- app/src/main/java/coom/hr/opendag/MainActivity.java
+++ app/src/main/java/coom/hr/opendag/MainActivity.java
@@ -2,6 +2,9 @@ package coom.hr.opendag;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Spinner;
@@ -10,20 +13,42 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     Spinner spinner;
+    private RecyclerView rv;
+    private boolean ignoreFirst = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Init recyclerview
+        // Lookup the recyclerview in activity layout
+        final LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        rv = findViewById(R.id.rv);
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(rv.getContext(), linearLayoutManager.getOrientation());
+        rv.addItemDecoration(dividerItemDecoration);
+
         spinner = findViewById(R.id.spinner);
-        //spinner.setOnItemSelectedListener();
 
             spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 
                 @Override
                 public void onItemSelected(AdapterView<?> parent, View view,  int pos, long id) {
-                    Toast.makeText(parent.getContext(), "OnItemSelectedListener : " + parent.getItemAtPosition(pos).toString(),
-                            Toast.LENGTH_SHORT).show();
+
+                    if (ignoreFirst) {
+                        ignoreFirst = false;
+                        return;
+                    }
+
+                    rv.setVisibility(View.VISIBLE);
+
+                    // Create adapter passing in the sample user data
+                    MainListAdapter adapter = new MainListAdapter(ActivityData.generateData(parent.getItemAtPosition(pos).toString()));
+                    // Attach the adapter to the recyclerview to populate items
+                    rv.setAdapter(adapter);
+                    // Set layout manager to position the items
+                    rv.setLayoutManager(linearLayoutManager);
+
                 }
 
                 @Override
